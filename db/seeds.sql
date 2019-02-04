9e1ba8d2cf79069ef934d330a0fe319c13fc8cb4
bareseph <barryriveroll@gmail.com>
2019-02-04 18:33:35 -0500

    Add directory structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Tuna Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Protein Style Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Western BBQ Burger", false);
